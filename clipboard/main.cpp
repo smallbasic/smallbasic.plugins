eddb9d9b5646acb36d7b1428e789f6e7014e464f
Chris Warren-Smith <cwarrensmith@gmail.com>
2020-07-07 22:04:28 +1000

    CLIPBOARD: add clipboard module

--- clipboard/main.cpp
+++ clipboard/main.cpp
@@ -23,7 +23,7 @@ void sblib_close(void) {
   clipboard_free(clipboard);
 }
 
-// clipboard.paste(text)
+// clipboard.copy(text)
 int sblib_proc_count() {
   return 1;
 }
@@ -31,7 +31,7 @@ int sblib_proc_count() {
 int sblib_proc_getname(int index, char *proc_name) {
   int result;
   if (index < sblib_proc_count()) {
-    strcpy(proc_name, "PASTE");
+    strcpy(proc_name, "COPY");
     result = 1;
   } else {
     result = 0;
@@ -42,15 +42,15 @@ int sblib_proc_getname(int index, char *proc_name) {
 int sblib_proc_exec(int index, int argc, slib_par_t *params, var_t *retval) {
   int result;
   if (index == 0 && argc == 1 && is_param_str(argc, params, 0)) {
-    const char *text = get_param_str(0, params, 0, "");
-    result = clipboard_set_text(clipboard, text);
+    const char *text = get_param_str(argc, params, 0, "");
+    result = clipboard_set_text_ex(clipboard, text, -1, LCB_CLIPBOARD);
   } else {
     result = 0;
   }
   return result;
 }
 
-// let text = clipboard.copy()
+// let text = clipboard.paste()
 int sblib_func_count() {
   return 1;
 }
@@ -58,7 +58,7 @@ int sblib_func_count() {
 int sblib_func_getname(int index, char *proc_name) {
   int result;
   if (index < sblib_func_count()) {
-    strcpy(proc_name, "COPY");
+    strcpy(proc_name, "PASTE");
     result = 1;
   } else {
     result = 0;
@@ -69,7 +69,7 @@ int sblib_func_getname(int index, char *proc_name) {
 int sblib_func_exec(int index, int argc, slib_par_t *params, var_t *retval) {
   int result;
   if (index == 0 && argc == 0) {
-    char *text = clipboard_text(clipboard);
+    char *text = clipboard_text_ex(clipboard, NULL, LCB_CLIPBOARD);
     if (text != nullptr) {
       v_setstr(retval, text);
       free(text);
