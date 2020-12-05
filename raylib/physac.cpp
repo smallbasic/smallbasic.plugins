aa25bb824d22c4ea8643dd6b849cae4130838bf6
Chris Warren-Smith <cwarrensmith@gmail.com>
2020-12-05 18:13:48 +1000

    RAYLIB: add physac.cpp

---
+++ raylib/physac.cpp
@@ -0,0 +1,16 @@
+// This file is part of SmallBASIC
+//
+// Plugin for raylib games library - https://www.raylib.com/
+//
+// This program is distributed under the terms of the GPL v2.0 or later
+// Download the GNU Public License (GPL) from www.gnu.org
+//
+// Copyright(C) 2020 Chris Warren-Smith
+
+#include "config.h"
+
+#include <raylib/raylib/src/raylib.h>
+#define PHYSAC_IMPLEMENTATION
+#define PHYSAC_NO_THREADS
+#include <raylib/src/physac.h>
+
